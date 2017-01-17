56026173780a305fe102e77c56e92962b5ea4215
Alexander Phelps <alexander.phelps@zanderbrook.com>
2017-01-17 09:13:53 -0800

    Initial project from spring initializr with Web and Config Server

---
+++ src/main/java/com/zbrook/SpringCloudConfigForGitsApplication.java
@@ -0,0 +1,12 @@
+package com.zbrook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigForGitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigForGitsApplication.class, args);
+	}
+}
